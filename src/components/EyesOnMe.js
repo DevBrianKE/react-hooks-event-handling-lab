73e25a26e62f2d7686a728f1f58ac5d3896a78e5
DevBrianKE <kipchumbabrian47@gmail.com>
2025-04-10 14:31:06 +0300

    complete lab

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,15 @@
-// Code EyesOnMe Component Here
+function EyesOnMe() {
+    const handleFocus = () => {
+        console.log("Good!")
+    }
+
+    const handleblur = () => {
+        console.log("Hey! Eyes on me!")
+    }
+
+
+    return (
+        <button onFocus={handleFocus} onBlur={handleblur}>Eyes on me</button>
+    )
+}
+export default EyesOnMe
